b08fcc3bd8f70358d7b91842039b1d015f975c90
TheMaster112a <157278006+TheMaster112a@users.noreply.github.com>
2025-08-28 19:03:35 +0530

    Delete 1. Second Largest.py

--- 1. Second Largest.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def getSecondLargest(self, arr):
-        arr = list(set(arr))
-        arr = sorted(arr)
-        if len(arr) == 1:
-            return -1
-        return arr[-2]
