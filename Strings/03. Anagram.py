d37be628a886eff99d32b5c0d2fdb52b82721c18
TheMaster112a <157278006+TheMaster112a@users.noreply.github.com>
2025-09-04 22:29:45 +0530

    Create 03. Anagram.py

---
+++ Strings/03. Anagram.py
@@ -0,0 +1,9 @@
+class Solution:
+    def areAnagrams(self, s1, s2):
+        
+        if len(s1) != len(s2):
+            return False
+        s1 = sorted(s1)
+        s2 = sorted(s2)
+        return s1 == s2
+       
