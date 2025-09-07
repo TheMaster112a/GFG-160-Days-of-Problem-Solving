1c54eddac0b403bff5a315bfb7f8eab3134b48d0
TheMaster112a <157278006+TheMaster112a@users.noreply.github.com>
2025-09-07 17:12:25 +0530

    Create 04. Non Repeating Character.py

---
+++ Strings/04. Non Repeating Character.py
@@ -0,0 +1,13 @@
+class Solution:
+    def nonRepeatingChar(self,s):
+        d = {}
+        for i in s:
+            if i in d:
+                d[i] += 1
+            else:
+                d[i] = 1
+        for i in s:
+            if d[i] == 1:
+                return i
+        return '$'
+    
